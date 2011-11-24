fd4e5ebec13044e023b75e3251a9cbb734b7a270
psilva <>
2011-11-24 08:24:42 +0000

    changing store place for psilva

--- scripts/mount_store.sh
+++ scripts/mount_store.sh
@@ -1,6 +1,13 @@
 base_storedir="./store"
 #storeremote="pclip11.cern.ch:/localdata/`whoami`/"
 storeremote="pclip11.cern.ch:/localdata/"
+WHOAMI=`whoami`
+if [ "$WHOAMI"="psilva" ];
+    then
+    storeremote="maccms316.cern.ch:/Users/psilva/store/"
+fi
+echo "Mounting ${storeremote} for ${WHOAMI} @ ${base_storedir}"
+
 storedir="${base_storedir}"
 echo $storeremote
 if ( ! mount | grep ${storeremote} > /dev/null )
